d263603e24060100b379d3786fa91cef39a19343
yuhao.ju <heiwu805@hotmail.com>
2016-04-04 15:06:34 +0800

    test reply

--- index.js
+++ index.js
@@ -39,36 +39,11 @@ app.use('/wechat', wechat(weConfig, function(req, res, next) {
 	// 微信输入信息都在req.weixin上
 	var message = req.weixin
 
-	if (message.Content === 'diaosi') {
-		// 回复屌丝(普通回复)
-		res.reply('hehe ')
-	} else if (message.Content === 'text') {
-		//你也可以这样回复text类型的信息
-		res.reply({
-			content: 'text object',
-			type: 'text'
-		})
-	} else if (message.Content === 'hehe') {
-		// 回复一段音乐
-		res.reply({
-			type: "music",
-			content: {
-				title: "来段音乐吧",
-				description: "一无所有",
-				musicUrl: "http://mp3.com/xx.mp3",
-				hqMusicUrl: "http://mp3.com/xx.mp3",
-				thumbMediaId: "thisThumbMediaId"
-			}
-		})
+	if (message.Content === 'hehe') {
+		res.reply('...')
 	} else {
-		// 回复高富帅(图文回复)
-		res.reply([{
-			title: '你来我家接我吧',
-			description: '这是女神与高富帅之间的对话',
-			picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-			url: 'http://nodeapi.cloudfoundry.com/'
-		}])
-	}
+		res.reply('测试中...')
+    }
 }))
 
 app.get('/hello', function(req, res) {
