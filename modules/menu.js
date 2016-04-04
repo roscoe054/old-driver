2d69d390b70c66545928f167319190b16665cd4a
yuhao.ju <heiwu805@hotmail.com>
2016-04-04 16:37:34 +0800

    fix menu link

--- modules/menu.js
+++ modules/menu.js
@@ -2,10 +2,10 @@ module.exports = {
     "button": [{
         "type": "view",
         "name": "预定",
-        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect'
+        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=reserve#wechat_redirect'
     }, {
         "type": "view",
         "name": "我的",
-        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect'
+        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=reserve#wechat_redirect'
     }]
 }
