91beaf618d73aaa799165da01a079346fe0419f7
yuhao.ju <heiwu805@hotmail.com>
2016-04-09 13:55:16 +0800

    add msgHandler

--- index.js
+++ index.js
@@ -25,60 +25,15 @@ var menuModel = require('./modules/menu')
 var api = new WechatAPI(weConfig.appid, weConfig.secret)
 api.createMenu(menuModel, function(){})
 
-// oauth
-var OAuth = require('wechat-oauth')
-var client = new OAuth(weConfig.appid, weConfig.secret)
-
 // view engine
 app.set('view engine', 'html')
 app.engine('html', require('ejs').renderFile)
 app.set('views', path.join(__dirname, 'views'))
 
 // auto reply
+var msgHandler = require('./modules/msgHandler')
 app.use('/wechat', wechat(weConfig, function(req, res, next) {
-	var msg = req.weixin
-
-	console.log(msg);
-
-    if(msg.MsgType === 'event' && msg.Event === 'subscribe'){
-        var bindingUrl = client.getAuthorizeURL('http://roscoe.cn/info', 'binding', 'snsapi_userinfo');
-        res.reply('等你好久了！<a href="' + bindingUrl + '">点击这里</a>以完成绑定(ง •_•)ง')
-        next()
-    }
-
-    if(msg.MsgType === 'text'){
-        if (msg.Content === 'hehe') {
-    		res.reply('...')
-    	} else {
-    		res.reply('这学期暂时不开啦，不好意思〒▽〒')
-        }
-        next()
-    }
-
-	if(msg.MsgType === 'voice'){
-		var recognition = msg.Recognition
-
-		// TODO remove this test
-		recognition = '提醒一下我明天上午十点开会'
-
-		if(recognition && recognition !== ""){
-			api.semantic(msg.FromUserName, {
-				query: recognition,
-				category: 'remind',
-				city: '上海'
-			}, function(err, result){
-				if(err){
-					res.reply('不好意思 服务器出了点小问题(´_ゝ`)')
-				} else{
-					res.reply(JSON.stringify(result.semantic))
-				}
-				next()
-			})
-		} else{
-			res.reply('不好意思 没有听清(´_ゝ`)')
-			next()
-		}
-	}
+	msgHandler(req, res, next)
 }))
 
 // router
---
+++ modules/msgHandler.js
@@ -0,0 +1,50 @@
+var weConfig = require('../config')
+var OAuth = require('wechat-oauth')
+var WechatAPI = require('wechat-api')
+
+var client = new OAuth(weConfig.appid, weConfig.secret)
+var api = new WechatAPI(weConfig.appid, weConfig.secret)
+
+module.exports = function(req, res, next){
+    var msg = req.weixin
+
+    if(msg.MsgType === 'event' && msg.Event === 'subscribe'){
+        var bindingUrl = client.getAuthorizeURL('http://roscoe.cn/info', 'binding', 'snsapi_userinfo');
+        res.reply('等你好久了！<a href="' + bindingUrl + '">点击这里</a>以完成绑定(ง •_•)ง')
+        next()
+    }
+
+    if(msg.MsgType === 'text'){
+        if (msg.Content === 'hehe') {
+    		res.reply('...')
+    	} else {
+    		res.reply('这学期暂时不开啦，不好意思〒▽〒')
+        }
+        next()
+    }
+
+	if(msg.MsgType === 'voice'){
+		var recognition = msg.Recognition
+
+		// TODO remove this test
+		recognition = '提醒一下我明天上午十点开会'
+
+		if(recognition && recognition !== ""){
+			api.semantic(msg.FromUserName, {
+				query: recognition,
+				category: 'remind',
+				city: '上海'
+			}, function(err, result){
+				if(err){
+					res.reply('不好意思 服务器出了点小问题(´_ゝ`)')
+				} else{
+					res.reply('已设置提醒：' + JSON.stringify(result.semantic))
+				}
+				next()
+			})
+		} else{
+			res.reply('不好意思 没有听清(´_ゝ`)')
+			next()
+		}
+	}
+}
