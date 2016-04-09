aadc927df41bf2ec99fa3feb3aa63c57e428ccf1
yuhao.ju <heiwu805@hotmail.com>
2016-04-10 02:06:49 +0800

    fix url

--- modules/menu.js
+++ modules/menu.js
@@ -4,6 +4,6 @@ module.exports = {
     "button": [{
         "type": "view",
         "name": "预定",
-        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=' + reserveUrl + '&response_type=code&scope=snsapi_userinfo&state=reserve#wechat_redirect'
+        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://115.159.119.199:8080/meeting/src/html/app.html#search&response_type=code&scope=snsapi_userinfo&state=reserve#wechat_redirect'
     }]
 }
