6828a585de65a67b501b0bc796d45cb337436709
yuhao.ju <heiwu805@hotmail.com>
2016-04-10 02:20:38 +0800

    fix url

--- modules/menu.js
+++ modules/menu.js
@@ -1,10 +1,7 @@
-var jumpPage = 'http://baidu.com'
-
-
 module.exports = {
     "button": [{
         "type": "view",
         "name": "预定",
-        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=' + jumpPage + '#wechat_redirect'
+        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=#wechat_redirect'
     }]
 }
