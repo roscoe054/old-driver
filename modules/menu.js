5371702f8b0e6e9018dd87f1873c5d5a03dff2b1
yuhao.ju <heiwu805@hotmail.com>
2016-04-10 02:05:57 +0800

    fix url

--- modules/menu.js
+++ modules/menu.js
@@ -1,4 +1,4 @@
-var reserveUrl = 'http://115.159.119.199:8080/meeting/src/html/app.html#search'
+var reserveUrl = encodeURIComponent('http://115.159.119.199:8080/meeting/src/html/app.html#search')
 
 module.exports = {
     "button": [{
