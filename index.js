ceb4d87fbcbdeb3c3c421172fae7aa3187e67017
yuhao.ju <heiwu805@hotmail.com>
2016-04-09 13:03:35 +0800

    test 语义

--- index.js
+++ index.js
@@ -62,14 +62,17 @@ app.use('/wechat', wechat(weConfig, function(req, res, next) {
 		recognition = '提醒一下我明天上午十点开会'
 
 		if(recognition && recognition !== ""){
-			api.semantic(msg.FromUserName, {
-				query: recognition,
-				category: 'remind',
-				city: '上海'
-			}, function(result){
-				res.reply(JSON.stringify(result.semantic))
-				next()
-			})
+			// api.semantic(msg.FromUserName, {
+			// 	query: recognition,
+			// 	category: 'remind',
+			// 	city: '上海'
+			// }, function(result){
+			// 	res.reply(JSON.stringify(result.semantic))
+			// 	next()
+			// })
+
+			res.reply('hehe')
+			next()
 		} else{
 			res.reply('不好意思 没有听清(´_ゝ`)')
 			next()
