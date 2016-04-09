68aaf0c82bac88ab7d447e6012ba08e1bf2be583
yuhao.ju <heiwu805@hotmail.com>
2016-04-10 02:09:50 +0800

    fix url

--- modules/menu.js
+++ modules/menu.js
@@ -1,9 +1,9 @@
-var reserveUrl = encodeURIComponent('http://115.159.119.199:8080/meeting/src/html/app.html#search')
+var jumpPage = encodeURIComponent('http://115.159.119.199:8080/meeting/src/html/app.html#search')
 
 module.exports = {
     "button": [{
         "type": "view",
         "name": "预定",
-        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://115.159.119.199:8080/meeting/src/html/app.html#search&response_type=code&scope=snsapi_userinfo&state=reserve#wechat_redirect'
+        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=' + jumpPage + '#wechat_redirect'
     }]
 }
