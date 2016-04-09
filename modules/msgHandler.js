cb29e51ff2672b2abf66bb9a0528a2763b6577d2
yuhao.ju <heiwu805@hotmail.com>
2016-04-10 04:32:43 +0800

    test semantic

--- modules/msgHandler.js
+++ modules/msgHandler.js
@@ -52,10 +52,11 @@ module.exports = function(req, res, next) {
                         }
                     })
 				} else {
+					var semantic = result.semantic.details
 					var remindText = '已设置提醒：\n'
-                        + result.semantic.datetime.date_ori
-                        + result.semantic.datetime.time_ori + '：'
-                        + result.semantic.event + '\n\n'
+                        + semantic.datetime.date_ori
+                        + semantic.datetime.time_ori + '：'
+                        + semantic.event + '\n\n'
                         + '如需取消提醒请<a href="#">点击这里</a>'
 
 					res.reply(remindText)
