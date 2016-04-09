a6c47e115b58822b44ecff62fbbb48aa8955f497
yuhao.ju <heiwu805@hotmail.com>
2016-04-10 04:03:43 +0800

    fix menu

--- modules/menu.js
+++ modules/menu.js
@@ -1,7 +1,11 @@
 module.exports = {
     "button": [{
         "type": "view",
-        "name": "预定",
-        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=#wechat_redirect'
+        "name": "订会议室",
+        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=search#wechat_redirect'
+    }, {
+        "type": "view",
+        "name": "我的会议",
+        "url": 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx65cfe45c2c6fad4a&redirect_uri=http://roscoe.cn/info&response_type=code&scope=snsapi_userinfo&state=reservation#wechat_redirect'
     }]
 }
